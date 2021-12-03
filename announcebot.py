da507ed3db199b10c68d1c09b47b4d82f00f4f05
Olivia Schaefer <taygetea@gmail.com>
2021-12-02 19:23:45 -0500

    what i have so far

--- announcebot.py
+++ announcebot.py
@@ -3,49 +3,63 @@ from typing import Optional, Union
 from aiohttp import web
 from forest.core import Bot, Message, Response, app
 import logging
+import phonenumbers as pn
+import csv
+import os
+
 
 class AnnounceBot(Bot):
-    # def __init__(self):
-    #     ##Init any relevant stuff
-    #     pass
+    #def __init__(self):
+         ##Init any relevant stuff
+        #self.pwd = os.getcwd()
+        #pass
+         # we need a dir of files of each user's collections, organized somehow
+         # 
 
-    async def subscriber_manager(self, method=None, number=None):
-        if method == "announce":
-            pass
-        ##Call out to subscriber database, write number
-        ##Simply an idea template, no need to use actual method
 
-    
 
-    async def handle_message(self, message: Message):
-        ##Custom message handling logic goes here
-        pass
 
-    async def do_announce(self, msg: Message):
-        logging.info(msg.full_text)
-        logging.info(msg.text)
+    # async def do_announce(self, msg: Message):
+    #     logging.info(msg.full_text)
+    #     logging.info(msg.text)
 
-        ##This would be an annouce command
-        return "What is returned will be the annoucement text"
+    #     ##This would be an annouce command
+    #     return "What is returned will be the annoucement text"
 
-    async def do_get_past_announcements(self, message: Message):
-        # Logic to get past annoucement
-        return "Annoucements List"
+    # async def do_get_past_announcements(self, message: Message):
+    #     # Logic to get past annoucement
+    #     return "Annoucements List"
 
-    async def do_subscribe(self, msg: Message):
-        try:
-            receipt = await subscriber_manager(number=message.source)
-            return "here's a link to the crypto renissance twitch signal channel"
-        except ConnectionError as e:
-            return "Subscription error! Please try again"
+    # async def do_subscribe(self, msg: Message):
+    #     try:
+    #         receipt = await subscriber_manager(number=message.source)
+    #         return "here's a link to the crypto renissance twitch signal channel"
+    #     except ConnectionError as e:
+    #         return "Subscription error! Please try again"
+
+    async def do_pn(self, msg: Message) -> str:
+        logging.info(msg)
+        if not msg.attachments:
+            return "attach a CSV of phone numbers"
+        else:
+            relname = msg.attachments[0]['fileName']
+            filename = os.getcwd() + "/downloads/" + relname
+            with open(filename, 'r') as f:
+                output = f.read()
+
+            return relname + '\n' + output
+        
 
     async def do_echo(self, msg: Message) -> str:
         """/imagine <prompt>"""
-        #logging.info(msg.full_text)
+        logging.info(msg)
+        logging.info(msg.full_text)
         logging.info(msg.text)
         logging.info("reached the echo codepath")
 
         return f"you said {msg.text}"
+
+
 
 if __name__ == "__main__":
 
